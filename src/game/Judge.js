6ac6941cd844348f11d6fd8e104e91eb3fb45770
perran <blank>
2017-06-08 00:53:14 +0200

    added test and some basic matching for the judge. please clean up on aisle 4!

--- src/game/Judge.js
+++ src/game/Judge.js
@@ -1,10 +1,121 @@
-var Judge = (function()
+class Judge
 {
-	function Judge()
+	constructor()
 	{
 	}
 	
-	ItemFactory.prototype.getColumnCombinations = function(matrix)
+	getVerticalCombinations(matrix, compareFunction, compareTo, size)
+	{
+		let matches = [];
+		
+		let width = matrix.length;
+		for(let x = 0; x < width; x++)
+		{
+
+			let column = matrix[x];
+			
+			for(let y = 0; y < column.length-(size-1); y++)
+			{
+				let isMatch = false;
+				for(let num = 0; num < size; num++)
+				{
+					let currentItem = column[y+num];
+					let currentComparer = compareFunction(currentItem);
+					
+					if(currentComparer === compareTo)
+					{
+						isMatch = true;
+					}
+					else
+					{
+						isMatch = false;
+						break;
+					}
+				}
+				
+				if(isMatch === true)
+				{
+					let match = column.slice(y, y+size);
+					matches.push(match);
+					
+					//step on to skip currently matched items
+					y += (size - 1); //-1 to handle the auto increment
+				}
+			}
+		}
+		
+		return matches;
+	}
+	
+	getHorizontalCombinations(matrix, compareFunction, compareTo, size)
+	{
+		let matches = [];
+		
+		let height = matrix[0].length;
+		
+		for(let y = 0; y < height; y++)
+		{
+			let width = matrix.length;
+			for(let x = 0; x < width - (size - 1); x++)
+			{
+				let combo = [];
+				
+				for(let num = 0; num < size; num++)
+				{
+					let currentItem = matrix[x+num][y];
+					if(compareFunction(currentItem) === compareTo)
+					{
+						combo.push(currentItem);
+					}
+				}
+				
+				if(combo.length === size)
+				{
+					matches.push(combo);
+				}
+			}
+		}
+		
+		return matches;
+	}
+	
+	_examineCombination(arr, index, compareFunction, compareTo)
+	{
+		for(let num = 0; num < size; num++)
+		{
+			let currentItem = column[y+num];
+			let currentComparer = compareFunction(currentItem);
+			
+			if(currentComparer === compareTo)
+			{
+				isMatch = true;
+			}
+			else
+			{
+				isMatch = false;	
+			}
+			
+			
+			if(isMatch === false)
+			{
+				break;
+			}
+		}
+	}
+	
+	_runMan(arr, index, compareFunction, compareTo)
+	{
+		let currentItem = arr[index];
+		let currentComparer = compareFunction(currentItem);
+		
+		if(currentComparer === compareTo)
+		{
+			return true;
+		}
+		return false;
+	}
+	
+	getColumnCombinations(matrix)
 	{
 		var width = this.matrix.length;
 	
@@ -18,7 +129,7 @@ var Judge = (function()
 				
 				if(remainingWidth < minimalComboSize)
 				{
-					traverseRemainingRow
+					this._traverseRemainingRow()
 				}
 				else
 				{
@@ -29,7 +140,7 @@ var Judge = (function()
 		}
 	}
 	
-	Judge.prototype.traverseRemainingRow = function(item, itemX, itemY)
+	_traverseRemainingRow(item, itemX, itemY)
 	{
 		var minimalComboSize = 3;
 				
@@ -60,6 +171,6 @@ var Judge = (function()
 		}
 		
 		return combo;
-	};
+	}
 	
-})();
+}
---
+++ test/game/JudgeTest.js
@@ -0,0 +1,57 @@
+describe("Judge", function()
+{
+	let judge;
+
+	beforeEach(function(){
+		judge = new Judge();
+	});
+
+	//get match max N
+	//collect those matches
+	//get all unique elements from those matches
+	//use these ignore matches where less than current N are included
+	//collect those new matches where not all where ignored
+	//continue to lowest match
+	
+	let createColumn = function(colorArray){
+		let arr = [];
+		for(var i = 0; i < colorArray.length; i++){
+			arr.push(new Item(colorArray[i], null));
+		}
+		
+		return arr;
+	}
+	
+	
+	describe("when match 3", function(){
+		
+		let c0 = createColumn([Color.Red, Color.Red, Color.Red, Color.Green, Color.Green, Color.Blue]);
+		let c1 = createColumn([Color.Blue, Color.Red, Color.Red, Color.Red, Color.Green, Color.Green]);
+		let c2 = createColumn([Color.Blue, Color.Green, Color.Red, Color.Red, Color.Red, Color.Green]);
+		let c3 = createColumn([Color.Blue, Color.Green, Color.Green, Color.Red, Color.Red, Color.Red]);
+		let c4 = createColumn([Color.Yellow, Color.Green, Color.Blue, Color.Green, Color.Blue, Color.Green]);
+		
+		let matrix = [c0, c1, c2, c3, c4];
+		
+		it("should return all vertical combinations", function(){
+		
+			let combos = judge.getVerticalCombinations(matrix, function(item){return item.getColor()}, Color.Red, 3);
+			
+            expect(combos.length).toBe(4);
+            expect(combos[0][0]).toBe(c0[0]);
+            expect(combos[0][1]).toBe(c0[1]);
+            expect(combos[0][2]).toBe(c0[2]);			
+		});
+		
+		it("should return all horizontal combinations", function(){
+			
+			let combos = judge.getHorizontalCombinations(matrix, function(item){return item.getColor()}, Color.Red, 3);
+			
+            expect(combos.length).toBe(2);
+            expect(combos[0][0]).toBe(c0[2]);
+            expect(combos[0][1]).toBe(c1[2]);
+            expect(combos[0][2]).toBe(c2[2]);			
+		});
+		
+	});
+});
